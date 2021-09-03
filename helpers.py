14d22f4d178838c7bd50fb7fba0a43bb59af6115
willcommit <welfversson@gmail.com>
2021-09-03 15:11:00 +0200

    adds venv folder to gitignore

--- helpers.py
+++ helpers.py
@@ -1,5 +1,5 @@
 import logging
-from apscheduler.schedulers.background import BackgroundScheduler
+from p.schedulers.background import BackgroundScheduler
 from shutil import copyfile
 from datetime import datetime
 import atexit
